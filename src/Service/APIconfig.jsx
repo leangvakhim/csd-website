f751c0439b8d0f32d0727acaa190309e8c498f3b
Khim070 <Vakhiml60@gmail.com>
2025-05-01 04:10:54 +0700

    Research

--- src/Service/APIconfig.jsx
+++ src/Service/APIconfig.jsx
@@ -177,12 +177,8 @@ const API_ENDPOINTS = {
     // developersocial
     getSocialDeveloper: `${API_BASEURL}/developersocial`,
 
-<<<<<<< Updated upstream
-   
-=======
     // researchtitle
     getResearchTitle: `${API_BASEURL}/rsd-title`,
->>>>>>> Stashed changes
 
     // subcontact
     getSubContact: `${API_BASEURL}/subcontact`,
