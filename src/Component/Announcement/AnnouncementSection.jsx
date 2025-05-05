dd980c81ddcde316b3352cdcdb44c9aa0550b46a
Khim070 <Vakhiml60@gmail.com>
2025-05-05 19:39:16 +0700

    Apply multilanguages on Announcement

--- src/Component/Announcement/AnnouncementSection.jsx
+++ src/Component/Announcement/AnnouncementSection.jsx
@@ -108,6 +108,7 @@ const AnnouncementSection = ({ section, menuLang }) => {
               id: announcement.am_id ?? null,
               title: announcement.am_title ?? '',
               description: announcement.am_shortdesc ?? '',
+              ref_id: announcement.ref_id ?? '',
               date: formattedDate,
               imageUrl: announcement?.img?.img
                 ? `${BASE_IMAGE_URL}/${announcement.img.img}`
@@ -133,6 +134,7 @@ const AnnouncementSection = ({ section, menuLang }) => {
       item.description.toLowerCase().includes(debouncedSearchTerm.toLowerCase());
     return matchesSearch;
   });
+
 
   const handleClearSearch = () => setSearchTerm('');
 
@@ -213,9 +215,9 @@ const AnnouncementSection = ({ section, menuLang }) => {
           {filteredNews.length > 0 ? (
             filteredNews.map((item) => (
               <div
-                key={item.id}
+                key={item.ref_id}
                 className="bg-white rounded-lg flex flex-col lg:flex-row shadow-md overflow-hidden cursor-pointer"
-                onClick={() => navigate(`/announcement/${item.id}`)}
+                onClick={() => navigate(`/announcement/${item.ref_id}`)}
               >
                 <div className="p-3 w-full lg:w-1/2 h-full">
                   <img
